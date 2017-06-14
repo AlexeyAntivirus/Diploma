28108f732ce15f542f654991bc93ac584e28a2d1
misha-grigorov <misha-grigorov@users.noreply.github.com>
2017-06-14 19:58:51 +0300

    ui enhancements: *added general error page * 404 page

--- src/main/java/com/rx/controllers/advices/FileDownloadControllerAdvice.java
+++ src/main/java/com/rx/controllers/advices/FileDownloadControllerAdvice.java
@@ -2,13 +2,12 @@ package com.rx.controllers.advices;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class FileDownloadControllerAdvice {
@@ -17,8 +16,22 @@ public class FileDownloadControllerAdvice {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handleException(Exception e) {
+    public ModelAndView handleException(Exception e) throws Exception {
         LOGGER.warn("Unexpected error appeared!", e);
-    }
 
+        // If the exception is annotated with @ResponseStatus rethrow it and let
+        // the framework handle it - like the OrderNotFoundException example
+        // at the start of this post.
+        // AnnotationUtils is a Spring Framework utility class.
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+
+        // Otherwise setup and send the user to a default error-view.
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("error");
+
+        return modelAndView;
+    }
 }
