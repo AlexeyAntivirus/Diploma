7b5dcb619e68620089c0429aaf97c6d148060985
jrunix <grigorovalexeyv@gmail.com>
2017-06-14 22:39:33 +0300

    * logout

---
+++ src/main/java/com/rx/controllers/LogoutController.java
@@ -0,0 +1,13 @@
+package com.rx.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "redirect:/login";
+    }
+}
